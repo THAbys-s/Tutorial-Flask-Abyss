874075f85d0362cf19fbaa753dd0184f89050410
Juan Parrado <jyunyauya@gmail.com>
2025-05-07 10:50:27 -0300

    Integré con render_template el uso de las template.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template #Importa render-template y la función url_for.
 import sqlite3
 from random import random
 app = Flask(__name__)
@@ -61,98 +61,118 @@ def division(n1, n2):
 
 # SQLITE 
 
-db = None  # Variable global para almacenar la conexión a la base de datos
+db = None  
 
-# Función para abrir la conexión a la base de datos
+
 def abrirConexion():
-    global db  # Hace referencia a la variable global db
-    db = sqlite3.connect("instance/datos.sqlite")  # Conecta a la base de datos SQLite
-    db.row_factory = sqlite3.Row  # Configura el cursor para devolver filas como diccionarios
-    return db  # Devuelve la conexión abierta
+    global db  
+    db = sqlite3.connect("instance/datos.sqlite")  # Es la conexión con la base de datos de SQLite.
+    db.row_factory = sqlite3.Row  
+    return db  
 
-# Función para cerrar la conexión a la base de datos
+
 def cerrarConexion():
-    global db  # Hace referencia a la variable global db
-    if db is not None:  # Verifica si la conexión está abierta
-        db.close()  # Cierra la conexión a la base de datos
-        db = None  # Establece db a None para indicar que no hay conexión activa
+    global db  
+    if db is not None:  
+        db.close()  
+        db = None  
 
-# Función para convertir filas de resultados de la base de datos a diccionarios
+
 def dict_factory(cursor, row):
   """Arma un diccionario con los valores de la fila."""
-  fields = [column[0] for column in cursor.description]  # Obtiene los nombres de las columnas
-  return {key: value for key, value in zip(fields, row)}  # Crea un diccionario con los nombres de columna como claves y los valores de la fila como valores
+  fields = [column[0] for column in cursor.description]  
+  return {key: value for key, value in zip(fields, row)}  
 
-# Ruta para obtener todos los usuarios de la base de datos
+
 @app.route("/sqlite/usuario")
 def obtenerGente():
-    global db  # Hace referencia a la variable global db
-    conexion = abrirConexion()  # Abre la conexión con la base de datos
-    cursor = conexion.cursor()  # Crea un cursor para ejecutar la consulta SQL
-    cursor.execute('SELECT * FROM usuarios')  # Ejecuta la consulta para seleccionar todos los usuarios
-    resultado = cursor.fetchall()  # Obtiene todos los resultados de la consulta
-    cerrarConexion()  # Cierra la conexión con la base de datos
-    fila = [dict(row) for row in resultado]  # Convierte cada fila de resultados a diccionario
-    return str(fila)  # Devuelve los resultados como una cadena de texto
+    global db  
+    conexion = abrirConexion()  
+    cursor = conexion.cursor()  
+    cursor.execute('SELECT * FROM usuarios')  
+    resultado = cursor.fetchall()  
+    cerrarConexion()  
+    fila = [dict(row) for row in resultado]  # Convierte cada fila en un diccionario
+    return str(fila)  
 
-# Ruta para contar cuántos registros hay en la tabla de usuarios
+
 @app.route("/sqlite/registro")
 def testRG():
-   global db  # Hace referencia a la variable global db
-   abrirConexion()  # Abre la conexión con la base de datos
-   cursor = db.cursor()  # Crea un cursor para ejecutar la consulta SQL
-   cursor.execute("SELECT COUNT(*) AS cant FROM usuarios; ")  # Ejecuta una consulta para contar los registros
-   res = cursor.fetchone()  # Obtiene el primer resultado (el número de registros)
-   registros = res["cant"]  # Extrae el valor del conteo
-   cerrarConexion()  # Cierra la conexión con la base de datos
-   return f"Hay {registros} registros en la tabla usuarios"  # Devuelve el conteo de registros
+   global db  
+   abrirConexion()  
+   db = db.cursor()  
+   db.execute("SELECT COUNT(*) AS cant FROM usuarios; ")  
+   res = db.fetchone()  
+   registros = res["cant"]  
+   cerrarConexion()  
+   return f"Hay {registros} registros en la tabla usuarios"  # Indica la cantidad de registros
 
-# Ruta para insertar un nuevo usuario en la base de datos
+
 @app.route("/sqlite/<string:usuario>/<string:email>")
 def testDB(usuario, email):
- conexion = abrirConexion()  # Abre la conexión con la base de datos
- cursor = conexion.cursor()  # Crea un cursor para ejecutar la consulta SQL
- # Insertamos de forma segura utilizando parámetros en lugar de concatenar valores
+ conexion = abrirConexion()  
+ db = conexion.cursor()  
+ 
  db.execute("INSERT INTO usuarios (usuario, email) VALUES (?, ?)", (usuario, email))  
- conexion.commit()  # Guarda los cambios realizados en la base de datos
- # Verificamos cuántos registros hay ahora
- cursor.execute("SELECT COUNT(*) as cant FROM usuarios")  # Consulta el número de registros
- res = cursor.fetchone()  # Obtiene el resultado de la consulta
+ conexion.commit()  
+ 
+ db.execute("SELECT COUNT(*) as cant FROM usuarios")  
+ res = db.fetchone()  
  cerrarConexion()  # Cierra la conexión con la base de datos
- registros = res["cant"]  # Extrae el número de registros
+ registros = res["cant"]  
  return f"Se insertó a {usuario} con email {email}. Ahora hay {registros} registros en la tabla usuarios."  # Devuelve un mensaje de confirmación
 
-# Ruta para actualizar un email en base al usuario en la base de datos
+
 @app.route("/sqlite/actualizar/<string:usuario>/<string:nuevo_email>")
 def testUpdate(usuario, nuevo_email):
- conexion = abrirConexion()  # Abre la conexión con la base de datos
- cursor = conexion.cursor()  # Crea un cursor para ejecutar la consulta SQL
- # Actualizmos de forma segura utilizando parámetros en lugar de concatenar valores.
- db.execute("UPDATE usuarios SET email=? WHERE usuario=?", (nuevo_email, usuario))  
- db.commit()  # Guarda los cambios realizados en la base de datos
- cerrarConexion()  # Cierra la conexión con la base de datos
- return f"Actualizamos el correo del usuario {usuario} a {nuevo_email}."  # Devuelve un mensaje de confirmación
+ conexion = abrirConexion()  
+ db = conexion.cursor()  
 
+ db.execute("UPDATE usuarios SET email=? WHERE usuario=?", (nuevo_email, usuario))  
+ db.commit()  
+ cerrarConexion() 
+ return f"Actualizamos el correo del usuario {usuario} a {nuevo_email}." 
 
 # Ruta para borrar un usuario de la base de datos por su id
 @app.route("/sqlite/delete/<int:id>")
 def testDelete(id):
- # Abre la conexión con la base de datos
  conexion = abrirConexion()
- cursor = conexion.cursor()  # Crea un cursor para ejecutar la consulta SQL
- # Borramos la fila de forma segura utilizando parámetros en lugar de concatenar valores
+ db = conexion.cursor() 
  db.execute("DELETE FROM usuarios WHERE id=?", (id,))
- conexion.commit()  # Guarda los cambios realizados en la base de datos
- cerrarConexion()  # Cierra la conexión con la base de datos
- return f"Se borro el id {id} en la tabla usuarios."  # Devuelve un mensaje de confirmación
-#by juanma
+ db.commit()  
+ cerrarConexion()  
+ return f"Se borro el id {id} en la tabla usuarios."  
+
 # Ruta para obtener un usuario específico de la base de datos por su id
 @app.route("/sqlite/usuario/<int:id>")
 def selecciónIndividual(id):
- conexion = abrirConexion()  # Abre la conexión con la base de datos
- cursor = conexion.cursor()  # Crea un cursor para ejecutar la consulta SQL
- cursor.execute('SELECT * FROM usuarios WHERE id=?', (id,))  # Ejecuta la consulta para obtener el usuario con el id dado
- resultado = cursor.fetchone()  # Obtiene el primer resultado de la consulta
- cerrarConexion()  # Cierra la conexión con la base de datos
- fila = dict(resultado)  # Convierte la fila de resultados en un diccionario
- return str(fila)  # Devuelve el resultado como una cadena de texto
+ conexion = abrirConexion()  
+ db = conexion.cursor()  
+ db.execute('SELECT * FROM usuarios WHERE id=?', (id,))  
+ resultado = db.fetchone()  # Extrae el primer resultado de la consulta anterior.
+ cerrarConexion()  
+ fila = dict(resultado)  
+ return str(fila)  # Retorna el resultado como un string.
+
+# Ruta con Template
+
+@app.route('/sqlite/mostrar-datos/<int:id>')
+def datos_sqlite(id):
+   abrirConexion()
+   cursor = db.cursor()
+   cursor.execute('SELECT id, usuario, email, telefono, direccion FROM usuarios WHERE id = ?; ', ((id,)))
+   res = cursor.fetchone()
+   cerrarConexion()
+   usuario = None
+   email = None
+   telefono = None
+   calle = None
+
+   if res != None:
+      usuario = res['usuario']
+      email = res['email']
+      telefono = res['telefono']
+      calle = res['direccion']
+      return render_template("template1.html", id=id, usuario=usuario, email=email, telefono=telefono, direccion=calle)
+
+
