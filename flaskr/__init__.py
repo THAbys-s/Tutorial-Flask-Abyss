62266af4d34643d5d0c73af708c4de035be55451
Juan Parrado <jyunyauya@gmail.com>
2025-05-21 12:05:26 -0300

    Aún me quedan cambios por hacer

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,7 +8,8 @@ def create_app(test_config=None):
 
     # configuración y setup...
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='SYBBAHT', #Flask lo utiliza para encriptar información importante 
+        # (Entre ellos el id, usuario y contraseña. Es muy importante cambiarlo)
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
@@ -18,15 +19,21 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs(app.instance_path) #Instancia una carpeta en la ruta proporcionada.
     except OSError:
-        pass
+        pass # Si ya está creada, simplemente no hace nada.
 
+    #Importo la conexión entre las distintas bibliotecas.
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
 
     return app
+
+# Una vista es la función que retorna Flask.
